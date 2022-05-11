53a69c52fcb6bf3a0200c8b712fe11e0fa9a5036
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2022-05-12 01:16:10 +0500

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,58 @@
+// Assignment no 1
+
+// question no 1
+alert("Hello Talha");
+
+// question no 2
+alert("Error! Please Enter A Valid Password");
+
+// question no 3
+alert("Welcome To JS land \n Happy Coding!");
+
+// question no 4
+alert("Welcome To JS land");
+alert("Happy Coding!");
+
+// question no 5
+alert("Hello... I can run JS through my web browser's console");
+
+
+// Assignment no 2
+
+// questio no 1
+var  useranme
+
+// questio no 2
+var myname = "Muhammad Talha"
+
+// questio no 3
+
+var message = "Hello world"
+alert(message);
+
+// questio no 4
+
+var name = "Jhone Doe"
+alert(name);
+
+var age = "15 years old"
+alert(age);
+
+var certificate = "Certified Mobile Application Development"
+alert(certificate);
+
+
+// question no 6
+var email = "tk411635@gmail.com"
+alert("My email is" +  email); 
+
+// question no 7
+
+var book = "A smarter way to learn JavaScript"
+alert("I am trying to learn the book" + book);
+
+// question no 8
+document.write("yah! i can write HTML through JS");
+// question no 9
+// var input = “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
+// alert(input);
